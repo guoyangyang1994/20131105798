4e18e1860ac8cb106947f77ab7005f5b52f725ad
guoyangyang1994 <a20131105798@admindeiMac-12.local>
2016-04-18 21:13:46 +0800

    yingwenban

--- 计算器 iOS 版本/计算器.swift
+++ 计算器 iOS 版本/计算器.swift
@@ -12,58 +12,58 @@
 
 import Foundation
 
-struct 计算 {
-    private var 当前算法:算法 = .未选择
+struct calculation {
+    private var Currentalgorithm:algorithm = .Nochoice
     
     
-    mutating func 设置当前算法(临时:算法) {
-        当前算法 = 临时
+    mutating func Setcurrentalgorithm(temporary:algorithm) {
+        Currentalgorithm = temporary
     }
     
-    func 读取当前算法() -> 算法! {
-        return 当前算法
+    func Readthecurrentalgorithm() -> algorithm! {
+        return Currentalgorithm
     }
     
     
     
     
-    enum 算法 {
-        case 加法
-        case 减法
-        case 乘法
-        case 除法
-        case 平方
-        case 指数幂
-        case 未选择
+    enum algorithm {
+        case addition
+        case subtraction
+        case multiplication
+        case division
+        case pingfang
+        case zhishumi
+        case Nochoice
     }
     
-    func 求结果(操作数:Double, 被操作数:Double) ->String {
-        var 返回 = ""
-        switch 当前算法 {
-        case .加法:
-            返回 = "\(操作数 + 被操作数)"
-        case .减法:
-            返回 = "\(操作数 - 被操作数)"
-        case .乘法:
-            返回 = "\(操作数 * 被操作数)"
-        case .除法 where 被操作数 != 0 :
-            返回 = "\(操作数 / 被操作数)"
-        case .平方:
-            返回 = "\(操作数 * 操作数)"
-        case .指数幂:
-            var 临时 = 操作数
-            let 整形被操作数 = Int(被操作数)
-            for _ in 1..<整形被操作数 {
-                临时 *= 操作数
+    func Resultseeking(Operationnumber:Double, Operandoperand:Double) ->String {
+        var revert = ""
+        switch Currentalgorithm {
+        case .addition:
+            revert = "\(Operationnumber + Operandoperand)"
+        case .subtraction:
+            revert = "\(Operationnumber - Operandoperand)"
+        case .multiplication:
+            revert = "\(Operationnumber * Operandoperand)"
+        case .division where Operandoperand != 0 :
+            revert = "\(Operationnumber / Operandoperand)"
+        case .pingfang:
+           revert = "\(Operationnumber * Operationnumber)"
+        case .zhishumi:
+            var temporary = Operationnumber
+            let intOperandoperand = Int(Operandoperand)
+            for _ in 1..<intOperandoperand {
+                temporary *= Operationnumber
             }
-            返回 = "\(临时)"
-        case .未选择:
-            返回 = "未选择算法！"
+           revert = "\(temporary)"
+        case .Nochoice:
+            revert = "未选择算法！"
         default:
-            返回 = "0 不能做被除数！"
+            revert = "0 不能做被除数！"
             
         }
-        return 返回
+        return revert
         
     }
 }
